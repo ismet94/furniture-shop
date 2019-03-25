3b1feebaec439bd7c2eec66ff2a65df003febe52
ismet94 <ismetsamso94@gmail.com>
2019-03-25 23:31:24 +0200

    Added pagenation and updated readme

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,37 @@
+$(function() {
+	$('ul.nav a').bind('click',function(event){
+		var $anchor = $(this);
+		/*
+		if you want to use one of the easing effects:
+		$('html, body').stop().animate({
+			scrollLeft: $($anchor.attr('href')).offset().left
+		}, 1500,'easeInOutExpo');
+		 */
+		$('html, body').stop().animate({
+			scrollLeft: $($anchor.attr('href')).offset().left
+		}, 1000);
+		event.preventDefault();
+	});
+});
+
+
+$(function() {
+	$('ul.nav a').bind('click',function(event){
+		var $anchor = $(this);
+
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500,'easeInOutExpo');
+		/*
+		if you don't want to use the easing effects:
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1000);
+		*/
+		event.preventDefault();
+	});
+});
+
 // let items = document.getElementByClassName('item');
 // console.log(items);
 
