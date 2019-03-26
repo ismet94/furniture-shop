b04922c35ca5b9ac76e4900d924577c898dda898
ismet94 <ismetsamso94@gmail.com>
2019-03-26 14:32:34 +0200

    added javascript to shop.html

--- js/javascript.js
+++ js/javascript.js
@@ -1,93 +1,64 @@
-$(function() {
-	$('ul.nav a').bind('click',function(event){
-		var $anchor = $(this);
-		/*
-		if you want to use one of the easing effects:
-		$('html, body').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-		}, 1500,'easeInOutExpo');
-		 */
-		$('html, body').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-		}, 1000);
-		event.preventDefault();
-	});
-});
-
-
-$(function() {
-	$('ul.nav a').bind('click',function(event){
-		var $anchor = $(this);
-
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500,'easeInOutExpo');
-		/*
-		if you don't want to use the easing effects:
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1000);
-		*/
-		event.preventDefault();
-	});
-});
 
 // let items = document.getElementByClassName('item');
-// console.log(items);
+// console.log(items)
 
-let cart = [
-    // ( name: name, price: price,),
-    // ( name: name, price: price,),
-    // ( name: name, price: price,),
-    // ( name: name, price: price,),
-    // ( name: name, price: price,),
-    // ( name: name, price: price,),
-];
+document.getElementById("showing-result1").innerHTML = "My First JavasdasdScript";
 
-function addCart(name, price) {
-    // create item
-    let item = {
-        name: name,
-        price: price,
-    };
+// let cart = [
+//     // ( name: name, price: price,),
+//     // ( name: name, price: price,),
+//     // ( name: name, price: price,),
+//     // ( name: name, price: price,),
+//     // ( name: name, price: price,),
+//     // ( name: name, price: price,),
+// ];
 
-    // check array if empty
-    if (cart.length != 0) {
-        let check = false;
-        for (let cartItem in cart) {
-            if (cart[cartItem].name === item.name) {
-                check = true;
-            }
-        }
 
-    // if check returns false, item was not found in the array
-    if (!check) { cart.push(item) }
 
-    // else push to array
-    } else {
-        cart.push(item);
-    }
+// function addCart(name, price) {
+//     // create item
+//     let item = {
+//         name: name,
+//         price: price,
+//     };
 
-    // get cart div
-    let div = document.getElementById('cart');
-    // clear cart
-    div.innerHTML = "";
+//     // check array if empty
+//     if (cart.length != 0) {
+//         let check = false;
+//         for (let cartItem in cart) {
+//             if (cart[cartItem].name === item.name) {
+//                 check = true;
+//             }
+//         }
 
-    // show cart with updated array
-    for (let value in cart) {
+//     // if check returns false, item was not found in the array
+//     if (!check) { cart.push(item) }
 
-        // Create a <li> node
-        let node = document.createElement("LI");
-        // Create a text node
-        let textnode = document.createTextNode(cart[value].name);
+//     // else push to array
+//     } else {
+//         cart.push(item);
+//     }
 
-        // Append the text to <li>
-        node.appendChild(textnode);
+//     // get cart div
+//     let div = document.getElementById('cart');
+//     // clear cart
+//     div.innerHTML = "";
 
-        // Append <li> to <ul> with id="myList"
-        div.appendChild(node);
-    }
-}
+//     // show cart with updated array
+//     for (let value in cart) {
+
+//         // Create a <li> node
+//         let node = document.createElement("LI");
+//         // Create a text node
+//         let textnode = document.createTextNode(cart[value].name);
+
+//         // Append the text to <li>
+//         node.appendChild(textnode);
+
+//         // Append <li> to <ul> with id="myList"
+//         div.appendChild(node);
+//     }
+// }
 
 // STILL TO DO
 // remove an item from cart
@@ -95,3 +66,4 @@ function addCart(name, price) {
 // add a quantity to cart items
 // be able to increase and decrease the quantity
 // REMEMBER the cart total must update as quantity changes
+
