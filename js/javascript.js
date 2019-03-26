c36b6b4bf3b0a0a36396c81f4c086a6bfcc73e1e
ismet94 <ismetsamso94@gmail.com>
2019-03-27 00:52:28 +0200

    Created Lightbox slider for Lookbook

--- js/javascript.js
+++ js/javascript.js
@@ -1,36 +1,46 @@
-$(function() {
-	$('ul.nav a').bind('click',function(event){
-		var $anchor = $(this);
-		/*
-		if you want to use one of the easing effects:
-		$('html, body').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-		}, 1500,'easeInOutExpo');
-		 */
-		$('html, body').stop().animate({
-			scrollLeft: $($anchor.attr('href')).offset().left
-		}, 1000);
-		event.preventDefault();
-	});
-});
 
+// ------------- LOOKBOOK ---------------
 
-$(function() {
-	$('ul.nav a').bind('click',function(event){
-		var $anchor = $(this);
-
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1500,'easeInOutExpo');
-		/*
-		if you don't want to use the easing effects:
-		$('html, body').stop().animate({
-			scrollTop: $($anchor.attr('href')).offset().top
-		}, 1000);
-		*/
-		event.preventDefault();
-	});
-});
+// Below is the lookbook lightbox script
+function openlightbox() {
+    document.getElementById('lightbox1').style.display = "block";
+  }
+  
+//   This closes the lightbox
+function closelightbox() {
+    document.getElementById('lightbox1').style.display = "none";
+  }
+  
+//   Slideshow functionality
+var slideShow = 1;
+    previewSlides(slideShow);
+  
+function plusSlides(n) {
+    previewSlides(slideShow += n);
+}
+  
+function currentSlide(n) {
+    previewSlides(slideShow = n);
+}
+  
+function previewSlides(n) {
+    var x;
+    var slides = document.getElementsByClassName("hide-lightbox");
+    var dots = document.getElementsByClassName("preview");
+    // once cliked next slide would show
+    if (n > slides.length) {slideShow = 1}
+    // once cliked previous slide would show
+    if (n < 1) {slideShow = slides.length}
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+    }
+    for (x = 0; x < dots.length; x++) {
+        dots[x].className = dots[x].className.replace(" active", "");
+    }
+    
+    slides[slideShow-1].style.display = "block";
+    dots[slideShow-1].className += " active";
+}
 
 // let items = document.getElementByClassName('item');
 // console.log(items);
